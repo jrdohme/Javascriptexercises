51b68997c3d059a8ff0863e42b4ec59218089f28
Jack <jrdohme@gmail.com>
2021-05-26 14:44:51 -0600

    completed sumAll

--- javascript-exercises-main/sumAll/sumAll.js
+++ javascript-exercises-main/sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
 
+    var answer = 0;
+
+    if(typeof start !== 'number'){
+        return "ERROR";
+    }
+    if(typeof end !== 'number'){
+        return "ERROR";
+    }
+    if(end<0 || start <0){
+        return "ERROR";
+    }
+
+    if(end > start){
+        for(x=start;x<=end;x++){
+            answer = answer + x;
+        }
+    }else{
+        for(x=start;x>=end;x--){
+            answer = answer + x;
+        }
+    }
+
+    return answer;
 };
 
 module.exports = sumAll;
