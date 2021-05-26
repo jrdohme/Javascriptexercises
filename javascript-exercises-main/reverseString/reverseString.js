1a853a3244578d24c0607030078f328561ef85d5
Jack <jrdohme@gmail.com>
2021-05-26 10:32:36 -0600

    completed reverseString

--- javascript-exercises-main/reverseString/reverseString.js
+++ javascript-exercises-main/reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    var length = string.length;
+    var reverseString = '';
 
+    for(x=0;x<=length;x++){
+        reverseString = reverseString + string.substr(length-x,1);
+    }
+
+    return reverseString;
 };
 
 module.exports = reverseString;
