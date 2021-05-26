9232142439b9c11a4596f9080c07e6be0a29fe28
Jack <jrdohme@gmail.com>
2021-05-26 14:56:48 -0600

    completed tempConversion

--- javascript-exercises-main/tempConversion/tempConversion.js
+++ javascript-exercises-main/tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
+const ftoc = function(far) {
+  answer = (far - 32) * 5 / 9;
 
+  answerRounded = Math.round(answer * 10)/10;
+
+  return answerRounded;
 };
 
-const ctof = function() {
+const ctof = function(cel) {
+  answer = cel * 9/5+32;
 
+  answerRounded = Math.round(answer * 10)/10;
+  
+  return answerRounded;
 };
 
 module.exports = {
