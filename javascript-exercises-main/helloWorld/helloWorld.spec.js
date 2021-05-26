2bd2b13cb23b85face4bd41947cc58d0631006f5
Jack <jrdohme@gmail.com>
2021-05-26 10:14:46 -0600

    helloWorld complete

--- javascript-exercises-main/helloWorld/helloWorld.spec.js
+++ javascript-exercises-main/helloWorld/helloWorld.spec.js
@@ -1,5 +1,7 @@
-const helloWorld = require('./helloWorld');
+const helloWorld = require('./helloWorld.js');
 
-test('says "Hello, World!"', function() {
-  expect(helloWorld()).toBe("Hello, World!");
-});
+describe('Hello World!', function(){
+  test('says "Hello, World!"', function() {
+    expect(helloWorld()).toBe("Hello, World!");
+  });
+})
