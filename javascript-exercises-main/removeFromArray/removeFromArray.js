2c4fdf9758885caf1fc99bf24a0d28114eed6e1f
Jack <jrdohme@gmail.com>
2021-05-26 14:31:05 -0600

    completed removeFromArray

--- javascript-exercises-main/removeFromArray/removeFromArray.js
+++ javascript-exercises-main/removeFromArray/removeFromArray.js
@@ -1,5 +1,31 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, first, second, third, fourth) {
 
+    if( fourth == undefined){
+        if( third == undefined){
+            if(second == undefined){
+                if(first == undefined){
+                    return "ERROR";
+                } else{
+                    return arr.filter(function(ele){
+                        return ele !== first;
+                    });
+                }
+            }else{
+                return arr.filter(function(ele){
+                    return (ele !== first && ele !== second);
+                });
+
+            }
+        }else{
+            return arr.filter(function(ele){
+                return (ele !== first && ele !== second && ele!== third);
+            });
+        }
+    }else{
+        return arr.filter(function(ele){
+            return (ele !== first && ele !== second && ele!== third && ele !== fourth);
+        });
+    }
 };
 
 module.exports = removeFromArray;
