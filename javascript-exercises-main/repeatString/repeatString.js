1001ffd2d5bc983563663ee59f810a63a7615f5b
Jack <jrdohme@gmail.com>
2021-05-26 10:25:56 -0600

    completed repeatString

--- javascript-exercises-main/repeatString/repeatString.js
+++ javascript-exercises-main/repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, count) {
 
+    block = '';
+
+    if(count < 0){
+        return "ERROR";
+    }
+
+    for(x=0;x<count;x++){
+        block = block + string;
+    }
+
+    return block;
 };
 
 module.exports = repeatString;
